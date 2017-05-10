0852879b33469ef5951191704ca49fbd1b52081a
Yupanka <justbecauseitisthere@gmail.com>
2017-05-10 12:01:30 -0400

    Styling fixes

--- test/unit/specs/ViewPerson.spec.js
+++ test/unit/specs/ViewPerson.spec.js
@@ -13,9 +13,7 @@ describe('ViewPerson.vue', () => {
   // Added by Natalie
 
   it('should display correct default data', () => {
-
-  	// this verifies that the correct default values are displayed in the person data fields
-
+// this verifies that the correct default values are displayed in the person data fields
     const vm = new Vue({
       template: '<div><view-person></view-person></div>',
       components: { ViewPerson }
@@ -23,56 +21,52 @@ describe('ViewPerson.vue', () => {
     const defaultData = ViewPerson.data();
 
     expect(vm.$el.querySelector('#personDataName').value)
-    	.to.contain(defaultData.personData.name);
+    .to.contain(defaultData.personData.name);
     expect(vm.$el.querySelector('#personDataHeight').value)
-    	.to.contain(defaultData.personData.height);
+    .to.contain(defaultData.personData.height);
     expect(vm.$el.querySelector('#personDataMass').value)
-    	.to.contain(defaultData.personData.mass);
+    .to.contain(defaultData.personData.mass);
     expect(vm.$el.querySelector('#personDataHair').value)
-    	.to.contain(defaultData.personData.hair_color);
+    .to.contain(defaultData.personData.hair_color);
     expect(vm.$el.querySelector('#personDataEyes').value)
-    	.to.contain(defaultData.personData.eye_color);
+    .to.contain(defaultData.personData.eye_color);
     expect(vm.$el.querySelector('#personDataDOB').value)
-    	.to.contain(defaultData.personData.birth_year);
+    .to.contain(defaultData.personData.birth_year);
     expect(vm.$el.querySelector('#personDataGender').value)
-    	.to.contain(defaultData.personData.gender);
+    .to.contain(defaultData.personData.gender);
   });
 
   it('Should display new values if personData is changed', done => {
-
-  	// verifies that if we change the data the new values will be displayed
-
-  	var newPerson = {
-  		name: 'El Carim',
-        height: 'unstable',
-        mass: 'unstable',
-        hair_color: 'purple',
-        eye_color: 'purple',
-        birth_year: '1200',
-        gender: 'changing',
-  	};
-  	var ctor =  Vue.extend(ViewPerson);
-  	var vm = new ctor();
-  	vm.$mount();
-    const defaultData = ViewPerson.data();
+// verifies that if we change the data the new values will be displayed
+    const newPerson = {
+      name: 'El Carim',
+      height: 'unstable',
+      mass: 'unstable',
+      hair_color: 'purple',
+      eye_color: 'purple',
+      birth_year: '1200',
+      gender: 'changing'
+    };
+    const Ctor = Vue.extend(ViewPerson);
+    const vm = new Ctor();
+    vm.$mount();
     vm.personData = newPerson;
     Vue.nextTick(() => {
-    	expect(vm.$el.querySelector('#personDataName').value)
-    		.to.contain(newPerson.name);
-    	expect(vm.$el.querySelector('#personDataHeight').value)
-    		.to.contain(newPerson.height);
-    	expect(vm.$el.querySelector('#personDataMass').value)
-    		.to.contain(newPerson.mass);
-	    expect(vm.$el.querySelector('#personDataHair').value)
-	    	.to.contain(newPerson.hair_color);
-	    expect(vm.$el.querySelector('#personDataEyes').value)
-	    	.to.contain(newPerson.eye_color);
-	    expect(vm.$el.querySelector('#personDataDOB').value)
-	    	.to.contain(newPerson.birth_year);
-	    expect(vm.$el.querySelector('#personDataGender').value)
-	    	.to.contain(newPerson.gender);
-    	done();
-  	});
+      expect(vm.$el.querySelector('#personDataName').value)
+      .to.contain(newPerson.name);
+      expect(vm.$el.querySelector('#personDataHeight').value)
+      .to.contain(newPerson.height);
+      expect(vm.$el.querySelector('#personDataMass').value)
+      .to.contain(newPerson.mass);
+      expect(vm.$el.querySelector('#personDataHair').value)
+      .to.contain(newPerson.hair_color);
+      expect(vm.$el.querySelector('#personDataEyes').value)
+      .to.contain(newPerson.eye_color);
+      expect(vm.$el.querySelector('#personDataDOB').value)
+      .to.contain(newPerson.birth_year);
+      expect(vm.$el.querySelector('#personDataGender').value)
+      .to.contain(newPerson.gender);
+      done();
+    });
   });
-  
 });
